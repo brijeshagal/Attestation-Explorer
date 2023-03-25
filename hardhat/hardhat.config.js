96cb8df6cda162f246b4c06389f0802d689b556d
vampire-ab <brijeshagal@gmail.com>
2023-03-25 09:24:35 +0530

    Casual Push

---
+++ hardhat/hardhat.config.js
@@ -0,0 +1,13 @@
+require("@nomicfoundation/hardhat-toolbox");
+require('@openzeppelin/hardhat-upgrades');
+require('dotenv').config();
+ 
+module.exports = {
+  solidity: "0.8.17",
+  networks:{
+    goerli:{
+      url: "https://rpc.ankr.com/eth_goerli",
+      accounts: [`0x${process.env.PRIVATE_KEY}`]
+    }
+  }
+};
